e144a69990143249b5dc5e60d883da0530c4307a
walow596 <walow596@O-PLTW-105-06.WTHS.NET>
2018-09-20 19:01:05 -0500

    Added all classes

--- Snake/src/game/Game.java
+++ Snake/src/game/Game.java
@@ -1,9 +1,15 @@
 package game;
 
+import javax.swing.JFrame;
+
 public class Game {
 
+	public static JFrame window;
+	
 	public static void main(String[] args) {
-		
+		window = new JFrame();
+		window.setSize(500, 500);
+		window.setVisible(true);
 	}
 	
 }
---
+++ Snake/src/graphics/GameGraphics.java
@@ -0,0 +1,16 @@
+package graphics;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class GameGraphics extends JComponent{
+
+	public void onPaint(Graphics g) {
+		
+	}
+	
+	public static void paintSquares(Graphics g) {
+		
+	}
+}
