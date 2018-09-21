fe82dc538fde41799e5d980459adab55507c3780
walow596 <walow596@O-PLTW-105-06.WTHS.NET>
2018-09-20 19:25:43 -0500

    Added Key Listener

--- Snake/src/game/Game.java
+++ Snake/src/game/Game.java
@@ -2,17 +2,23 @@ package game;
 
 import javax.swing.JFrame;
 
+import backend.KeyboardListener;
 import data.Direction;
+import graphics.GameGraphics;
 
 public class Game {
 
 	public static JFrame window;
+	private static GameGraphics graphics;
 	
 	public static void main(String[] args) {
+		graphics = new GameGraphics();
+		
 		window = new JFrame();
 		window.setSize(500, 500);
 		window.setVisible(true);
-		window.addKeyListener(l);
+		window.addKeyListener(new KeyboardListener());
+		window.add(graphics);
 	}
 	
 }
--- Snake/src/graphics/GameGraphics.java
+++ Snake/src/graphics/GameGraphics.java
@@ -1,16 +1,24 @@
 package graphics;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
 
+import snake.Square;
+
 public class GameGraphics extends JComponent{
 
-	public void onPaint(Graphics g) {
+	public void paint(Graphics g) {
+		paintSquares(g);
 	}
 	
 	public static void paintSquares(Graphics g) {
+		Square[] squares = getSquares();
 		
+		for(Square square : squares) {
+			
+		}
 	}
 }
